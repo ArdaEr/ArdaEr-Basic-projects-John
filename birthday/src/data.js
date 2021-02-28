e3c5c2ebb0af298bd547b488a8fe383e5ca2ea58
ArdaEr <cengizhanarda98@gmail.com>
2021-02-28 04:18:31 +0300

    first commit

---
+++ birthday/src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+      id: 1,
+      name: 'Clara Oswald',
+      age: 35,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+      id: 2,
+      name: 'River Song',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Rory Williams',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Danny Pink',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Amy Pond',
+      age: 33,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
+  
